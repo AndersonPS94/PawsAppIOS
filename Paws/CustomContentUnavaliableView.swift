c8d10b03fddd37907264493e5ae28985ab5f1a13
AndersonPS <dson_adn@hotmail.com>
2025-09-01 15:23:59 -0300

    first commit

---
+++ Paws/CustomContentUnavaliableView.swift
@@ -0,0 +1,37 @@
+//
+//  CustomContentUnavaliableView.swift
+//  Paws
+//
+//  Created by Anderson Pereira Dos Santos on 01/09/25.
+//
+
+import SwiftUI
+
+struct CustomContentUnavaliableView: View {
+    var icon: String
+    var title: String
+    var description: String
+    
+
+    var body: some View {
+        ContentUnavailableView {
+            Image(systemName: icon)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 96)
+            
+            Text(title)
+                .font(.title)
+        } description: {
+            Text(description)
+        }
+        .foregroundStyle(.tertiary)
+    }
+}
+
+#Preview {
+    CustomContentUnavaliableView(
+        icon: "cat.circle",
+        title:"No Photo" ,
+        description: "Add a photo to get started. ")
+}
