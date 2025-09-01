d82ea6db7fa42acc4b924fa1c12d08199374c177
AndersonPS <dson_adn@hotmail.com>
2025-09-01 11:41:42 -0300

    Initial Commit

---
+++ Paws/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Paws
+//
+//  Created by Anderson Pereira Dos Santos on 01/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Anderson Pereira Dos Santos on 01/09/25.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
